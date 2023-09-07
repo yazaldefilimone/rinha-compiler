30379a54fab3d03fe01f74e04534521bc0202621
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-09-08 01:34:40 +0200

    *

--- readme.txt
+++ readme.txt
@@ -1,2 +1,1 @@
-
 MIT © Yazalde Filimone
---
+++ src/evaluator.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+const ast = @import("ast");
+
+pub fn evaluator() void {
+    print("Hello, World!\n");
+}
