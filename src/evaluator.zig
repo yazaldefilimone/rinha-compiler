f7dc3b348052eed61a4ac050cd5d6cbccd18c6c9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-09-08 02:56:35 +0200

    feat: evaluator

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -2,6 +2,16 @@ const std = @import("std");
 const print = @import("std").debug.print;
 const ast = @import("ast");
 
-pub fn evaluator() void {
-    print("Hello, World!\n");
+pub fn evaluator(program: ast.Program) void {
+    const current_node = program;
+    const result = null;
+
+    while (current_node != null) {
+        result = eval_statement(current_node);
+        current_node = current_node.next;
+    }
+}
+
+fn optimize(program: ast.Program) ast.Program {
+    return program;
 }
